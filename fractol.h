ba6c4358c30f3309968181e4b7a162e22e8b191c
GabrielVSM <gabrielvitorsales@gmail.com>
2021-08-11 19:16:25 -0300

    add libraries to header

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/06 16:11:34 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/08/10 12:09:40 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/08/11 19:15:58 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 # define FRACTOL_H
 
 # include <math.h>
+# include <stdlib.h>
+# include <errno.h>
+# include <stdio.h>
+# include <string.h>
 # include "mlx.h"
 # include "libft.h"
 
