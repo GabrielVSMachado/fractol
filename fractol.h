365da2e70efa8cef156c668afd9377443457839a
GabrielVSM <gabrielvitorsales@gmail.com>
2021-08-12 20:03:38 -0300

    add define to errors of arguments

--- fractol.h
+++ fractol.h
@@ -6,12 +6,16 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/06 16:11:34 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/08/11 19:15:58 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/08/12 20:02:46 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
+# define TRUE 1
+# define FALSE 0
+# define ERROR_IN_REAL_PART "Real part are wrong!"
+# define ERROR_IN_IMG_PART "Imaginary part are wrong!"
 
 # include <math.h>
 # include <stdlib.h>
@@ -21,4 +25,23 @@
 # include "mlx.h"
 # include "libft.h"
 
+typedef struct s_complex
+{
+	double	real;
+	double	imaginary;
+}				t_complex;
+
+typedef struct s_fractol
+{
+	int			flag;
+	t_complex	z;
+}				t_fractol;
+
+enum e_sets
+{
+	mandelbrot = 1,
+	julia = 2
+};
+/** check inputs in argc and argv **/
+void	check_input(int	argc, char **argv, t_fractol *fractol);
 #endif
