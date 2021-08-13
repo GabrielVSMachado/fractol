552020455a36f4894dbfd7995f6f4b541f620c1e
GabrielVSM <gabrielvitorsales@gmail.com>
2021-08-13 13:57:55 -0300

    feat: add errors to wrong real and imaginary arguments

--- src/errors_handler.c
+++ src/errors_handler.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/13 12:27:35 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/08/13 13:25:23 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/08/13 13:57:22 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,15 @@ void	error_handler(int n_error, char **to_free)
 	{
 		if (n_error == AVAILABLE_SETS)
 			printf("The available sets are:\n mandelbrot;\n julia.\n");
-		else if (n_error == COMPLEX_ARGUMENTS_WRONGS)
+		else if (n_error == COMPLEX_ARGUMENTS)
 			printf("THE COMPLEX NUMBER MUST BE GIVEN IN TWO\
 					SEPARATE ARGUMENTS(2, 3). WHICH THE NUMBER 2 \
 					ARE THE REAL PART (+0.0 OR -0.0) AND THE NEXT \
 					ARE THE IMAGINARY PART(+0.0i OR -0.0i)\n");
+		else if (n_error == IMAGINARY_PART)
+			printf("IN FOURTH ARGUMENT USE ONE OF THIS FORMATS: +0.0i OR -0.0i\n");
+		else if (n_error == REAL_PART)
+			printf("IN THIRD ARGUMENT USE ONE OF THIS FORMATS: +0.0 OR -0.0\n");
 	}
 	else
 	{
