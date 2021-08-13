63f24ae8141481908005531ed81d8bfd32c3e04b
GabrielVSM <gabrielvitorsales@gmail.com>
2021-08-13 13:43:14 -0300

    change define name of COMPLEX_ARGUMENTS_WRONGS

--- errors.h
+++ errors.h
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/13 13:03:21 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/08/13 13:39:50 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/08/13 13:42:26 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define ERRORS_H
 
 # define AVAILABLE_SETS 1
-# define COMPLEX_ARGUMENTS_WRONGS 2
+# define COMPLEX_ARGUMENTS_WRONG 2
 
 /* show errors message on code */
 void	error_handler(int n_error, char **to_free);
