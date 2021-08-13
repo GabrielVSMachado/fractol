8311e43d5d24f982d28ddab9c0a5ceedfd7bfa8c
GabrielVSM <gabrielvitorsales@gmail.com>
2021-08-13 13:40:20 -0300

    feat: add prototype of error_handler

--- errors.h
+++ errors.h
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/13 13:03:21 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/08/13 13:32:36 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/08/13 13:39:50 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,6 @@
 # define AVAILABLE_SETS 1
 # define COMPLEX_ARGUMENTS_WRONGS 2
 
+/* show errors message on code */
+void	error_handler(int n_error, char **to_free);
 #endif
