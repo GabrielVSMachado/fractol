2c53ffd561caf5806cb7cc5186aa363752007097
GabrielVSM <gabrielvitorsales@gmail.com>
2021-08-13 14:00:52 -0300

    refactory: use function error_handler to handler with errors

--- src/check_input.c
+++ src/check_input.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/12 17:21:49 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/08/13 12:51:52 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/08/13 14:00:02 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,19 @@ static void	check_complex(char *a, char *b);
 
 void	check_input(int argc, char **argv, t_fractol *fractol)
 {
-	if (argc == 1)
-	{
-		printf(AVAILABLE_SETS);
-		exit(EXIT_FAILURE);
-	}
 	if (!ft_strcmp(argv[1], "mandelbrot"))
 		fractol->flag = mandelbrot;
 	else if (!ft_strcmp(argv[1], "julia"))
 	{
 		if (argc < 4)
-		{
-			printf(FEW_COMPLEX_ARGUMENTS);
-			exit(EXIT_FAILURE);
-		}
+			error_handler(COMPLEX_ARGUMENTS, NULL);
 		check_complex(argv[2], argv[3]);
 		fractol->flag = julia;
 		fractol->z.real = ft_atod(argv[2]);
 		fractol->z.imaginary = ft_atod(argv[3]);
 	}
 	else
-	{
-		printf(AVAILABLE_SETS);
-		exit(EXIT_FAILURE);
-	}
+		error_handler(AVAILABLE_SETS, NULL);
 }
 
 static void	check_complex(char *real_part, char *imaginary_part)
@@ -49,10 +38,7 @@ static void	check_complex(char *real_part, char *imaginary_part)
 			&& *real_part != '.' 
 			&& *real_part != '+'
 			&& *real_part != '-')
-		{
-			printf("%s\n USE ONE OF THIS FORMATS: +0.0 OR -0.0\n", strerror(EINVAL));
-			exit(EXIT_FAILURE);
-		}
+			error_handler(REAL_PART, NULL);
 		real_part++;
 	}
 	while (*imaginary_part)
@@ -62,10 +48,7 @@ static void	check_complex(char *real_part, char *imaginary_part)
 			&& *imaginary_part != '+'
 			&& *imaginary_part != '-'
 			&& *imaginary_part != 'i')
-		{
-			printf("%s\n USE ONE OF THIS FORMATS: +0.0i OR -0.0i\n", strerror(EINVAL));
-			exit(EXIT_FAILURE);
-		}
+			error_handler(IMAGINARY_PART, NULL);
 		imaginary_part++;
 	}
 }
