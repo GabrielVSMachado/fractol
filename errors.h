0dd84bc1114f8d7b3c23489c67602b3e0931e6d9
GabrielVSM <gabrielvitorsales@gmail.com>
2021-08-13 13:56:39 -0300

    add macros to Real and Imaginary parts of complex number

--- errors.h
+++ errors.h
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/13 13:03:21 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/08/13 13:42:26 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/08/13 13:56:09 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 # define ERRORS_H
 
 # define AVAILABLE_SETS 1
-# define COMPLEX_ARGUMENTS_WRONG 2
+# define COMPLEX_ARGUMENTS 2
+# define REAL_PART 3
+# define IMAGINARY_PART 4
 
 /* show errors message on code */
 void	error_handler(int n_error, char **to_free);
