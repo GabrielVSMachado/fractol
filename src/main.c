62bdcbf9d7ea00b89703a5c62b78442288cc796c
GabrielVSM <gabrielvitorsales@gmail.com>
2021-08-13 13:46:27 -0300

    feat: check if argc == 1 in main

--- src/main.c
+++ src/main.c
@@ -6,12 +6,18 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/07 08:20:16 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/08/07 09:23:09 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/08/13 13:45:51 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
+	t_fractol	fractol;
+
+	if (argc == 1)
+		error_handler(AVAILABLE_SETS, NULL);
+	check_input(argc, argv, &fractol);
+	return (0);
 }
