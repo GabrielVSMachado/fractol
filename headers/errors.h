83ac564d94df616f181847099c5685e04f6d91b6
GabrielVSM <gabrielvitorsales@gmail.com>
2021-08-13 17:43:40 -0300

    add macros to error and ok

--- headers/errors.h
+++ headers/errors.h
@@ -6,12 +6,14 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/13 13:03:21 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/08/13 13:56:09 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/08/13 17:42:50 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERRORS_H
 # define ERRORS_H
+# define ERROR -1
+# define OK 0
 
 # define AVAILABLE_SETS 1
 # define COMPLEX_ARGUMENTS 2
