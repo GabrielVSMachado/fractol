27edb3f88bd8b2a95a2a89eaefcf9f2f009c752c
GabrielVSM <gabrielvitorsales@gmail.com>
2021-08-13 18:13:17 -0300

    fix: logic of check imaginary part

--- src/check_input.c
+++ src/check_input.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/12 17:21:49 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/08/13 18:02:20 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/08/13 18:12:39 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	check_input(int argc, char **argv, t_fractol *fractol)
 		if (check_complex(argv[2]))
 			error_handler(REAL_PART, NULL);
 		fractol->z.real = ft_atod(argv[2]);
-		if (!ft_strrchr(argv[3], 'i') && check_complex(argv[3]))
+		if (!ft_strrchr(argv[3], 'i') || check_complex(argv[3]))
 			error_handler(IMAGINARY_PART, NULL);
 		if (*argv[3] == 'i' || (*argv[3] == '-' && *(argv[3] + 1) == 'i'))
 			fractol->z.imaginary = *argv[3] == 'i'
