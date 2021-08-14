9e83b5fbbdbb3274bb8ea94da92bcfb4497e8f34
GabrielVSM <gabrielvitorsales@gmail.com>
2021-08-14 14:17:59 -0300

    fix: output of errors messages

--- src/errors_handler.c
+++ src/errors_handler.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/13 12:27:35 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/08/13 14:13:03 by gvitor-s         ###   ########.fr       */
+/*   Created: 2021/08/14 14:14:34 by gvitor-s          #+#    #+#             */
+/*   Updated: 2021/08/14 14:15:02 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ void	error_handler(int n_error, char **to_free)
 	if (to_free == NULL)
 	{
 		if (n_error == AVAILABLE_SETS)
-			printf("The available sets are:\n mandelbrot;\n julia.\n");
+			printf("THE AVAILABLE SETS ARE:\n mandelbrot;\n julia.\n");
 		else if (n_error == COMPLEX_ARGUMENTS)
-			printf("THE COMPLEX NUMBER MUST BE GIVEN IN TWO\
-					SEPARATE ARGUMENTS(2, 3). WHICH THE NUMBER 2 \
-					ARE THE REAL PART (+0.0 OR -0.0) AND THE NEXT \
-					ARE THE IMAGINARY PART(+0.0i OR -0.0i)\n");
+			printf("THE COMPLEX NUMBER MUST BE GIVEN IN TWO \
+SEPARATE ARGUMENTS(2, 3).\nWHICH THE NUMBER 2 \
+ARE THE REAL PART (+0.0 OR -0.0) AND THE NEXT \
+ARE THE IMAGINARY PART(+0.0i OR -0.0i).\n");
 		else if (n_error == IMAGINARY_PART)
 			printf("IN FOURTH ARGUMENT USE ONE OF THIS \
-					FORMATS: +0.0i OR -0.0i\n");
+FORMATS: +0.0i OR -0.0i\n");
 		else if (n_error == REAL_PART)
 			printf("IN THIRD ARGUMENT USE ONE OF THIS FORMATS: +0.0 OR -0.0\n");
 	}
