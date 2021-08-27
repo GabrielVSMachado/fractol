50e0c4ef0c0b6e1a76b450caf0c1230f6de441d1
GabrielVSM <gabrielvitorsales@gmail.com>
2021-08-27 18:07:30 -0300

    refactory: move prototype of function errors to fractol header

--- headers/errors.h
+++ headers/errors.h
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/13 13:03:21 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/08/13 17:42:50 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/08/27 12:57:14 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@
 # define COMPLEX_ARGUMENTS 2
 # define REAL_PART 3
 # define IMAGINARY_PART 4
+# define GEN_IMG 5
 
-/* show errors message on code */
-void	error_handler(int n_error, char **to_free);
 #endif
