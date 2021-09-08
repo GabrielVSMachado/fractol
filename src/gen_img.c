19fdb3272f3503862f09921f1cdd108183081400
GabrielVSM <gabrielvitorsales@gmail.com>
2021-09-08 15:20:41 -0300

    feat: colors change as the factor grow in gen_img

--- src/gen_img.c
+++ src/gen_img.c
@@ -6,20 +6,20 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 13:02:04 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/09/03 18:00:09 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/09/08 15:13:59 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-static void	my_mlx_put_pixel(t_data *data, int x, int y, int color);
+static void	my_mlx_put_pixel(t_data *data, int x, int y, unsigned int n_iter);
 
 void	gen_img(t_fractol *fractol)
 {
-	int		x_axis;
-	int		y_axis;
-	int		n_iter;
-	int		pixel;
+	unsigned int		x_axis;
+	unsigned int		y_axis;
+	unsigned int		n_iter;
+	unsigned int		pixel;
 
 	pixel = 0;
 	while (pixel != WIDTH * HEIGHT)
@@ -27,22 +27,32 @@ void	gen_img(t_fractol *fractol)
 		x_axis = pixel % WIDTH;
 		y_axis = pixel / WIDTH;
 		fractol->z.re = fractol->offset_x
-			+ (((double)x_axis / WIDTH) * (4) * fractol->zoom);
+			+ (((double)x_axis / WIDTH) * (REAL_DISTANCE) * fractol->zoom);
 		fractol->z.im = fractol->offset_y
-			+ (((double)y_axis / HEIGHT) * (4) * fractol->zoom);
+			+ (((double)y_axis / HEIGHT) * (IMG_DISTANCE) * fractol->zoom);
 		n_iter = fractol->iter(fractol->z, fractol->c);
-		my_mlx_put_pixel(&fractol->img, x_axis, y_axis,
-			255 - (int)(n_iter * 255 / MAX_ITER));
+		my_mlx_put_pixel(&fractol->img, x_axis, y_axis, n_iter);
 		pixel++;
 	}
 	mlx_put_image_to_window(fractol->mlx, fractol->mlx_win, fractol->img.img,
 		0, 0);
 }
 
-static void	my_mlx_put_pixel(t_data *data, int x, int y, int color)
+static void	my_mlx_put_pixel(t_data *data, int x, int y, unsigned int n_iter)
 {
-	char	*dst;
+	char			*dst;
+	unsigned int	*color;
+	int				factor;
 
-	dst = data->addr + (y * data->line_lenght + x * (data->bits_p_pix / 8));
-	*(unsigned int *)dst = color;
+	dst = data->addr + ((y * data->line_lenght) + (x << (data->bits_p_pix >> 4)));
+	color = (unsigned int *)dst;
+	if (n_iter == MAX_ITER)
+		*color = 0xFF000000;
+	else
+	{
+			factor = (int)(255 * n_iter / MAX_ITER);
+			*color = 0xFF << 24;
+			*color |= (int)pow(factor, 1.5) << 8;
+			*color |= (255 - factor) << 16;
+	}
 }
