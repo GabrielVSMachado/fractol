174a1aa613d56dfa1214375e849b20a9d38997f1
GabrielVSM <gabrielvitorsales@gmail.com>
2021-09-08 16:04:45 -0300

    feat: function to init struct and pointer to julia_set calc in main

--- src/main.c
+++ src/main.c
@@ -6,11 +6,13 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/07 08:20:16 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/09/02 19:59:44 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/09/08 15:24:32 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+static void	init_struct(t_fractol *fractol);
 
 int	main(int argc, char *argv[])
 {
@@ -18,13 +20,20 @@ int	main(int argc, char *argv[])
 
 	if (argc == 1)
 		error_handler(AVAILABLE_SETS, NULL);
-	ft_bzero(&fractol, sizeof(t_fractol));
-	fractol.zoom = 1.0;
-	fractol.offset_x = REAL_INIT;
-	fractol.offset_y = IMG_INIT;
+	init_struct(&fractol);
 	check_input(argc, argv, &fractol);
 	if (fractol.flag == mandelbrot)
-		fractol.iter = iteration_mandelbrot;
+		fractol.iter = mandelbrot_set;
+	else if (fractol.flag == julia)
+		fractol.iter = julia_set;
 	init_mlx(&fractol);
 	return (EXIT_SUCCESS);
+}
+
+static void	init_struct(t_fractol *fractol)
+{
+	ft_bzero(fractol, sizeof(t_fractol));
+	fractol->zoom = 1.0;
+	fractol->offset_x = REAL_INIT;
+	fractol->offset_y = IMG_INIT;
 }
