8469c046c7f58b87d6fa981883092496a4b3270e
GabrielVSM <gabrielvitorsales@gmail.com>
2021-09-09 19:20:23 -0300

    refactory: functions receive max_iter ad parameter now in calc_fractol

--- src/calc_fractol.c
+++ src/calc_fractol.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 18:15:30 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/09/08 21:57:38 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/09/09 18:22:08 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,39 @@
 
 static double	complex_abs(t_complex z);
 
-int	mandelbrot_set(t_complex c, t_complex z)
+int	mandelbrot_set(t_complex c, t_complex z, int max_iter)
 {
 	double		tmp;
 	int			n_iter;
 
 	n_iter = 0;
-	while (complex_abs(z) <= 4 && n_iter < MAX_ITER)
+	while (complex_abs(z) <= 4 && n_iter < max_iter)
 	{
 		tmp = pow(z.re, 2) - pow(z.im, 2);
 		z.im = (2 * z.im * z.re) + c.im;
 		z.re = tmp + c.re;
 		n_iter++;
 	}
-	if (n_iter == MAX_ITER)
-		return (MAX_ITER);
+	if (n_iter == max_iter)
+		return (max_iter);
 	return (n_iter + 1 - log(log2(complex_abs(z))));
 }
 
-int	julia_set(t_complex z, t_complex c)
+int	julia_set(t_complex z, t_complex c, int max_iter)
 {
 	double	tmp;
 	int		n_iter;
 
 	n_iter = 0;
-	while (complex_abs(z) <= 4 && n_iter < MAX_ITER)
+	while (complex_abs(z) <= 4 && n_iter < max_iter)
 	{
 		tmp = pow(z.re, 2) - pow(z.im, 2);
 		z.im = (2 * z.im * z.re) + c.im;
 		z.re = tmp + c.re;
 		n_iter++;
 	}
-	if (n_iter == MAX_ITER)
-		return (MAX_ITER);
+	if (n_iter == max_iter)
+		return (max_iter);
 	return (n_iter + 1 - log(log2(complex_abs(z))));
 }
 
