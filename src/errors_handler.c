40449548cbe1e8b1aa5f94834a7cd70575595d44
GabrielVSM <gabrielvitorsales@gmail.com>
2021-09-12 22:05:53 -0300

    feat: check to mlx items, errors_handler

--- src/errors_handler.c
+++ src/errors_handler.c
@@ -12,12 +12,14 @@
 
 #include "fractol.h"
 
+static void	message_error(int n_error);
+
 void	error_handler(int n_error, t_fractol **to_free)
 {
 	if (to_free == NULL)
 	{
 		if (n_error == AVAILABLE_SETS)
-			printf("THE AVAILABLE SETS ARE:\n mandelbrot;\n julia.\n");
+			printf("THE AVAILABLE SETS ARE:\n mandelbrot;\n julia;\n mandelbar.\n");
 		else if (n_error == COMPLEX_ARGUMENTS)
 			printf("THE COMPLEX NUMBER MUST BE GIVEN IN TWO \
 SEPARATE ARGUMENTS(2, 3).\nWHICH THE NUMBER 2 \
@@ -31,8 +33,24 @@ FORMATS: +0.0i OR -0.0i\n");
 	}
 	else
 	{
-		free(*to_free);
+		if ((*to_free)->img.img)
+			mlx_destroy_image((*to_free)->mlx, (*to_free)->img.img);
+		if ((*to_free)->mlx_win)
+			mlx_destroy_window((*to_free)->mlx, (*to_free)->mlx_win);
+		if ((*to_free)->mlx)
+			mlx_destroy_display((*to_free)->mlx);
+		message_error(n_error);
 		printf("%s\n", strerror(ENOMEM));
 	}
 	exit (EXIT_FAILURE);
+}
+
+static void	message_error(int n_error)
+{
+	if (n_error == MLX_NULL)
+		perror("Mlx not initiate");
+	else if (n_error == MLX_IMG_NULL)
+		perror("Window not Initialized");
+	else if (n_error == MLX_IMG_NULL)
+		perror("Image is Null");
 }
